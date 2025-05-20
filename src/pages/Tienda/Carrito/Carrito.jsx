bfbd594846e33cf9a3c83f4391a4c3528ba18519
DavidVR27 <orchestraaa69@gmail.com>
2025-05-19 22:43:00 -0500

    Implementacion de Tailwind y Componentes

--- src/pages/Tienda/Carrito/Carrito.jsx
+++ src/pages/Tienda/Carrito/Carrito.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import './Carrito.css';
+import Boton from '../../../Components/Boton';
 
 const Carrito = ({ productos, total = 100, delivery = 10, descuentos = 5 }) => {
   const totalPagar = total + delivery - descuentos;
@@ -9,12 +10,14 @@ const Carrito = ({ productos, total = 100, delivery = 10, descuentos = 5 }) => {
     <div className="carrito-container">
       <Card style={{ width: '350px' }}>
         <Card.Body>
-          <Card.Title>Resumen de Carrito</Card.Title>
+          <Card.Title>Resumen de la compra</Card.Title>
           <p>Total: S/ {total}</p>
           <p>Delivery: S/ {delivery}</p>
-          <p>Descuentos: -S/ {descuentos}</p>
-          <p>Total a Pagar: S/ {totalPagar}</p>
-          <Button variant="primary">Continuar Compra</Button>
+          <p className='descuentos'>Descuentos: -S/ {descuentos}</p>
+          <p className='total-carrito'>Total: S/ {totalPagar}</p>
+          <Boton
+            texto={'Finalizar compra'}
+          ></Boton>
         </Card.Body>
       </Card>
     </div>
