c1bf86ce0648fe915a6dc2d97a2d134c174c16ee
DavidVR27 <orchestraaa69@gmail.com>
2025-05-19 22:09:21 -0500

    Test

--- src/pages/Tienda/Carrito/Carrito.jsx
+++ src/pages/Tienda/Carrito/Carrito.jsx
@@ -10,10 +10,10 @@ const Carrito = ({ productos, total = 100, delivery = 10, descuentos = 5 }) => {
       <Card style={{ width: '350px' }}>
         <Card.Body>
           <Card.Title>Resumen de Carrito</Card.Title>
-          <p>Total: S/{total}</p>
-          <p>Delivery: S/{delivery}</p>
-          <p>Descuentos: -S/{descuentos}</p>
-          <p>Total a Pagar: S/{totalPagar}</p>
+          <p>Total: S/ {total}</p>
+          <p>Delivery: S/ {delivery}</p>
+          <p>Descuentos: -S/ {descuentos}</p>
+          <p>Total a Pagar: S/ {totalPagar}</p>
           <Button variant="primary">Continuar Compra</Button>
         </Card.Body>
       </Card>
