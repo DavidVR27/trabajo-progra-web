e85e8e33f2d0aaf09f1a68209e9aad6b6e9da340
DavidVR27 <orchestraaa69@gmail.com>
2025-05-20 08:10:02 -0500

    Mejoras al carrito y su interfaz

--- src/pages/Tienda/Carrito/Carrito.jsx
+++ src/pages/Tienda/Carrito/Carrito.jsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import productosData from '../../../../public/productos.json'; // Importamos el JSON
 import Boton from '../../../Components/Boton';
 
-const Carrito = ({ actualizarTotal }) => {
+const Carrito = ({ actualizarTotal, actualizarTotalProductos }) => {
   const [productos, setProductos] = useState(
-    productosData.productos.map((producto) => ({ ...producto, cantidad: 1, checked: true }))
+    productosData.productos.map((producto) => ({ 
+      ...producto, 
+      cantidad: producto.cantidad || 1, 
+      checked: true 
+    }))
   );
 
   // Calcular el total del carrito
@@ -13,16 +17,23 @@ const Carrito = ({ actualizarTotal }) => {
     0
   );
 
-  // Actualizar el total en el componente padre (Checkout.jsx)
+  // Calcular el total de productos seleccionados
+  const totalProductos = productos.reduce(
+    (acc, producto) => acc + (producto.checked ? producto.cantidad : 0),
+    0
+  );
+
+  // Actualizar el total y el total de productos en el componente padre (Checkout.jsx)
   useEffect(() => {
     actualizarTotal(total);
-  }, [total, actualizarTotal]);
+    actualizarTotalProductos(totalProductos);
+  }, [total, totalProductos, actualizarTotal, actualizarTotalProductos]);
 
   const toggleCheck = (id) => {
     setProductos((prevProductos) =>
       prevProductos.map((producto) =>
         producto.id === id
-          ? { ...producto, checked: !producto.checked, cantidad: !producto.checked ? 1 : 0 }
+          ? { ...producto, checked: !producto.checked }
           : producto
       )
     );
@@ -41,42 +52,68 @@ const Carrito = ({ actualizarTotal }) => {
   const disminuirCantidad = (id) => {
     setProductos((prevProductos) =>
       prevProductos.map((producto) =>
-        producto.id === id && producto.cantidad > 0 && producto.checked
+        producto.id === id && producto.cantidad > 1 && producto.checked
           ? { ...producto, cantidad: producto.cantidad - 1 }
           : producto
       )
     );
   };
 
+  const eliminarProducto = (id) => {
+    setProductos((prevProductos) =>
+      prevProductos.filter((producto) => producto.id !== id)
+    );
+  };
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="flex flex-col space-y-4">
       {productos.map((producto) => (
         <div
           key={producto.id}
-          className="bg-white shadow-md rounded-lg p-4 flex flex-col items-start"
+          className="bg-white shadow-md rounded-lg p-4 flex items-start w-full"
         >
-          <div className="flex items-center justify-between w-full">
-            <h2 className="text-lg font-semibold">{producto.nombre}</h2>
+          <div className="flex items-center mr-4">
             <input
               type="checkbox"
-              className="form-checkbox h-5 w-5 text-green-500"
+              className="form-checkbox h-5 w-5 text-green-500 mr-2"
               checked={producto.checked}
               onChange={() => toggleCheck(producto.id)}
             />
+            <img src={producto.imagen} alt={producto.nombre} className="w-24 h-24 object-cover rounded-md" />
           </div>
-          <p className="text-gray-600">Precio: S/ {producto.precio}</p>
-          <p className="text-gray-600">Cantidad: {producto.cantidad}</p>
-          <div className="flex mt-4">
-            <Boton
-              onClick={() => disminuirCantidad(producto.id)}
-              disabled={!producto.checked || producto.cantidad <= 1}
-              texto={"-"}
-            />
-            <Boton 
-              onClick={() => incrementarCantidad(producto.id)}
-              disabled={!producto.checked}
-              texto={"+"}
-            />
+          <div className="flex-grow">
+            <div className="flex items-center justify-between w-full">
+              <h2 className="text-lg font-bold">{producto.nombre}</h2>
+              <p className="text-gray-600 font-bold text-xl">S/ {producto.precio}</p>
+            </div>
+            <p className="text-gray-600">{producto.descripcion}</p>
+            <p className="text-[#FE624C] font-bold">{producto.llegada}</p>
+            <div className="flex items-center mt-4 justify-end">
+              <div className="flex items-center">
+                <span className="mr-4">Cantidad</span>
+                <Boton
+                  onClick={() => disminuirCantidad(producto.id)}
+                  disabled={!producto.checked || producto.cantidad <= 1}
+                  texto={"-"}
+                  className="px-3 py-1 mr-2"
+                />
+                <span className="mx-3">{producto.cantidad}</span>
+                <Boton 
+                  onClick={() => incrementarCantidad(producto.id)}
+                  disabled={!producto.checked}
+                  texto={"+"}
+                  className="px-3 py-1 ml-2"
+                />
+              </div>
+              <button
+                onClick={() => eliminarProducto(producto.id)}
+                className="text-red-500 hover:text-red-700 ml-6"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       ))}
--- src/pages/Tienda/Carrito/Checkout.jsx
+++ src/pages/Tienda/Carrito/Checkout.jsx
@@ -6,27 +6,53 @@ import Boton from '../../../Components/Boton';
 
 
 const Checkout = () => {
-  const [total, setTotal] = useState(0); // Estado para almacenar el total del carrito
+    const [total, setTotal] = useState(0); // Estado para almacenar el total del carrito
+  const [totalProductos, setTotalProductos] = useState(0); // Estado para almacenar el total de productos
+  const [totalDescuento, setTotalDescuento] = useState(0); // Estado para almacenar el total de descuento
+
 
   return (
     <div className="bg-gray-100 min-h-screen p-6">
-      {/* Navbar */}
+    {/* Navbar */}
       <Navbar />
       {/* Sección principal */}
       <h1 className="text-2xl font-bold mb-6">Checkout</h1>
       <div className="flex flex-col lg:flex-row gap-6">
         {/* Sección izquierda: Productos */}
         <div className="flex-1 bg-white shadow-md rounded-lg p-4">
-          <Carrito actualizarTotal={setTotal} />
+          <div className="flex flex-col">
+            <Carrito 
+              actualizarTotal={setTotal} 
+              actualizarTotalProductos={setTotalProductos}
+              actualizarTotalDescuento={setTotalDescuento}
+            />
+          </div>
         </div>
 
         {/* Sección derecha: Resumen del carrito */}
         <div className="w-full lg:w-1/3 bg-white shadow-md rounded-lg p-4">
-          <h2 className="text-lg font-semibold mb-4">Resumen del Carrito</h2>
-          <p className="text-gray-600">Total: S/ {total}</p>
+          <h2 className="text-lg font-semibold mb-4">Resumen de la compra</h2>
+          <div className="flex justify-between mb-2">
+            <span>Productos ({totalProductos})</span>
+            <span>S/. {total.toFixed(2)}</span>
+          </div>
+          <div className="flex justify-between mb-2">
+            <span>Descuento</span>
+            <span><span className="text-red-500 font-bold">-S/ {totalDescuento.toFixed(2)}</span></span>
+          </div>
+          <div className="flex justify-between mb-2">
+            <span>Delivery</span>
+            <span><span className="text-green-500 font-bold">GRATIS</span></span>
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="flex justify-between text-gray-600 font-semibold text-xl mb-6">
+            <span>Total</span>
+            <span>S/ {total.toFixed(2)}</span>
+          </div>
           <Boton
             texto="Proceder al Pago"
             onClick={() => alert('Redirigiendo a la pasarela de pago...')} 
+            className="w-full py-3 text-lg"
           />
         </div>
       </div>
